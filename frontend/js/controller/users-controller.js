0a7dea07e68ed9d08a92439eaea02a3ad140cb5d
Carbone <heloisa.r.carbone@accenture.com>
2016-11-11 16:18:51 -0200

    fix

--- frontend/js/controller/users-controller.js
+++ frontend/js/controller/users-controller.js
@@ -1,35 +1,50 @@
 /**
  * Created by heloisa.r.carbone on 11/7/2016.
  */
-'use strict'
-
-desafio.controller('UsersController',
-    function UsersController($scope, usersData) {
+'use strict';
 
 
-        usersData.getUsers().success(function (users) {
-            console.log(users);
-            $scope.users = users;
-        });
+function UsersController($scope, usersData) {
 
-        $scope.changeUsersData = function() {
-            console.log($scope.newEntry)
-            $scope.users.users.push($scope.newEntry);
-            $scope.newEntry = "";
-        }
-
-        $scope.sortorder = "name";
-
-        $scope.sortBy = function (sortorder) {
-            $scope.changeFilterGender('all');
-            $scope.sortorder = sortorder;
-
-        };
-
-        $scope.filtergender = "all";
-
-        $scope.changeFilterGender = function (gender) {
-            $scope.filtergender = gender;
-        };
-        
+    usersData.getUsers().success(function (users) {
+        console.log(users);
+        $scope.users = users;
     });
+
+    $scope.changeUsersData = function () {
+        console.log($scope.newEntry);
+        $scope.users.users.push($scope.newEntry);
+        $scope.newEntry = {};
+    };
+
+    $scope.sortorder = "name";
+
+    $scope.sortBy = function (sortorder) {
+        $scope.changeFilterGender('all');
+        $scope.sortorder = sortorder;
+
+    };
+
+    $scope.cleanFields = function() {
+        $scope.newEntry = {};
+    };
+
+    $scope.genderList = [
+        {
+            id: 'male',
+            value: 'Masculino'
+        },
+        {
+            id: 'female',
+            value: 'Feminino'
+        }
+    ];
+
+    $scope.filtergender = "all";
+
+    $scope.changeFilterGender = function (gender) {
+        $scope.filtergender = gender;
+    };
+}
+
+angular.module('desafio').controller('UsersController', UsersController);
--- frontend/js/general.js
+++ frontend/js/general.js
@@ -1,40 +1,11 @@
-
-
-$(".dropdown-menu li").click(function(){
-    $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <i class="fa fa-chevron-down" aria-hidden="true"></i>');
-    $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
-});
-
-
-$(document).ready(function(){
-    var date_input=$('input[name="date"]'); //our date input has the name "date"
-    var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
-    var options={
+$(document).ready(function () {
+    var date_input = $('input[name="date"]'); //our date input has the name "date"
+    var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
+    var options = {
         format: 'dd/mm/yyyy',
         container: container,
         todayHighlight: true,
-        autoclose: true,
+        autoclose: true
     };
     date_input.datepicker(options);
-});
-
-function cleanFields() {
-    $('#nameInput').val("");
-    $('#date').click();
-    $("#gender").val('');
-}
-
-$("#searchbar").on("keyup", function() {
-    var value = $(this).val();
-
-    $(".body-table").each(function() {
-
-        if ($(this).text().indexOf(value) != -1) {
-            $(this).show();
-        }
-        else {
-            $(this).hide();
-        }
-
-    });
 });
